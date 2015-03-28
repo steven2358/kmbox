e9417cf74d7b38f168f315050213dbe227681a7a
Steven <steven2358@gmail.com>
2015-03-28 16:58:15 +0100

    add km_krr_icd function contributed by Agamemnon Krasoulis

--- demo/km_demo_krr_icd.m
+++ demo/km_demo_krr_icd.m
@@ -1,9 +1,6 @@
-% KM_KRR_DEMO Kernel ridge regression (also known as kernel least-squares 
-% (KLS) on data sampled from a sinc function.
-%
-% This program implements the example shown in Figure 2.1 of "Kernel
-% Methods for Nonlinear Identification, Equalization and Separation of
-% Signals", Ph.D. dissertation by S. Van Vaerenbergh.
+% KM_DEMO_KRR_ICD Kernel ridge regression (also known as kernel
+% least-squares KLS) on data sampled from a sinc function, using incomplete
+% Cholesky decomposition.
 %
 % Author: Steven Van Vaerenbergh (steven *at* gtas.dicom.unican.es), 2010
 %
@@ -15,38 +12,35 @@ clear all
 
 %% PARAMETERS
 
-N = 2500;			% number of data points sampled from sinc
-N2 = 100;		% number of data points for testing the regression
-nvar = 0.05;	% noise variance factor
+N = 2500; % number of data points sampled from sinc
+N2 = 100; % number of data points for testing the regression
+nvar = 0.05; % noise variance factor
 
-lambda = 1E-4;		% regularization constant
-kernel = 'gauss';	% kernel type
-sigma = 1;			% Gaussian kernel width
+lambda = 1E-4; % regularization constant
+kernel = 'gauss'; % kernel type
+sigma = 1; % Gaussian kernel width
+
+m = 100; % maximal rank of decomposition
 
 %% PROGRAM
 tic
 
-x = randn(N,1);	% sampled data
-n = nvar*randn(N,1);	% noise
-y = sin(3*x)./x+n;		% noisy sinc data
-x2 = linspace(-3,3,N2)';	% input data for testing the regression
+X = randn(N,1); % sampled data
+noise = nvar*randn(N,1); % noise
+Y = sin(3*X)./X+noise; % noisy sinc data
+X2 = linspace(-3,3,N2)'; % input data for testing the regression
 
-[G,subset] = km_kernel_icd(x,kernel,sigma,100,1E-6);
-K = km_kernel(x,x(subset,:),kernel,sigma);
-alpha = K\y;
+[alpha,Y2,subset] = km_krr_icd(X,Y,kernel,sigma,m,X2);
 
-K2 = km_kernel(x2,x(subset,:),kernel,sigma);
-y2 = K2*alpha;
-
-y2sinc = sin(3*x2)./x2;	% true sinc output data corresponding to x2
+Y2sinc = sin(3*X2)./X2; % true sinc output data corresponding to x2
 
 toc
 %% OUTPUT
 
 figure; hold on
-plot(x,y,'x','Color',[0.7,0.7,0.7]);
-plot(x2,y2sinc,'b--','LineWidth',2)
-plot(x2,y2,'r');
-plot(x(subset,:),y(subset),'og')
+plot(X,Y,'x','Color',[0.7,0.7,0.7]);
+plot(X2,Y2sinc,'b--','LineWidth',2)
+plot(X2,Y2,'r');
+plot(X(subset,:),Y(subset),'og')
 legend('noisy data','regression','true sinc function','support points')
 title('Kernel ridge regression demo')
